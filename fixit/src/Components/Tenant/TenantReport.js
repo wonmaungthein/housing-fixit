decfc75619bbaede405f94b74696dd5b7e7b7f7a
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-23 15:19:38 +0000

    created a draft page instruction

--- fixit/src/Components/Tenant/TenantReport.js
+++ fixit/src/Components/Tenant/TenantReport.js
@@ -6,6 +6,21 @@ export class TenantReport extends Component {
     return (
       <div>
         <h1>TenantReport Page</h1>
+        <section className="first-section">
+          <h3>
+            {" "}
+            We will put the drop down menu to select the issues icon here
+          </h3>
+        </section>
+        <section className="second-section">
+          <h3>A few blocks of options of issues the tenenat are facing</h3>
+        </section>
+        <section className="third-section">
+          <h3>Tanent information with name, email, and message</h3>
+        </section>
+        <section className="fourth-section">
+          <h3>Submit bottom</h3>
+        </section>
       </div>
     );
   }
