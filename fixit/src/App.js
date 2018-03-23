582d3f72a2a66d2e10a2b4bfb7bc2b7844df3f2c
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-23 20:07:47 +0000

    Added react dom

--- fixit/src/App.js
+++ fixit/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { LandLord } from "./Components/LandLord/LandLord";
 import { Letting } from "./Components/Letting/Letting";
-import { Agent } from "./Components/Agent/Agent"; 
-import Routes  from "./Components/Routes/Routes";
+import { Agent } from "./Components/Agent/Agent";
+import Routes from "./Components/Routes/Routes";
 import "./App.css";
-// import { TenantReport } from "./Components/Tenant/TenantReport";
+import { TenantReport } from "./Components/Tenant/TenantReport";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FeedBack } from "./Components/Tenant/FeedBack";
-// import { Acknoledge } from "./Components/LandLord/Acknoledge";
+import { Acknoledge } from "./Components/LandLord/Acknoledge";
 
 class App extends Component {
   render() {
@@ -18,18 +18,18 @@ class App extends Component {
           <h1 className="App-title">Welcome to Fix It</h1>
         </header>
         <Router>
-        <div>
-          <Routes />
-          <Route exact path="/" component={TenantReport} />
-          <Route exact path="/Letting" component={Letting} />
-          <Route exact path="/LandLord" component={LandLord} />
-          <Route exact path="/Agent" component={Agent}/> 
-        </div>
-      </Router>
+          <div>
+            <Routes />
+            <Route exact path="/" component={TenantReport} />
+            <Route exact path="/Letting" component={Letting} />
+            <Route exact path="/LandLord" component={LandLord} />
+            <Route exact path="/Agent" component={Agent} />
+          </div>
+        </Router>
         <p className="App-intro">
-          {/* <TenantReport /> */}
+          <TenantReport />
           {/* <Acknoledge /> */}
-          <FeedBack />
+          {/* <FeedBack /> */}
         </p>
       </div>
     );
