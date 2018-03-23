62352ae0735a98740d5ce0596144198a76dd52cd
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-23 15:08:28 +0000

    added first component

---
+++ fixit/src/Components/Tenant/TenantReport.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+export class TenantReport extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TenantReport Page</h1>
+      </div>
+    );
+  }
+}
